153d3e57d1f6ddb1d5c28adbac6e2189d7da5705
munagalavijaya <117559593+munagalavijaya@users.noreply.github.com>
2022-11-06 11:49:54 +0530

    Add files via upload
    day1

---
+++ day1/Mobile.java
@@ -0,0 +1,20 @@
+package week1.day1;
+public class Mobile {
+	public void makeCall()
+	{
+		System.out.println("my first program");
+	}
+public void sendMsg() {
+		System.out.println("hai everyone");
+	
+}
+	public static void main(String[] args) {
+		
+		Mobile msg=new Mobile();
+		
+		
+		msg.makeCall();
+		msg.sendMsg();
+	}
+
+}
