8faa9d948d7448f7d7cd005b219614232778205e
munagalavijaya <117559593+munagalavijaya@users.noreply.github.com>
2022-11-11 16:57:21 +0530

    Add files via upload

---
+++ Twowheeler.java
@@ -0,0 +1,23 @@
+package week1;
+
+public class Twowheeler {
+	
+	int noOfWheels =2;
+	short noOfMirrors =2;
+	long chassisNumber =123695455L;
+	boolean isPunctured=false;
+	String bikename="honda";
+	double runningkm=456d;
+
+	public static void main(String[] args) {
+		
+		Twowheeler bike=new Twowheeler();
+		System.out.println("bike.noOfWheels");
+		System.out.println("noOfMirrors");
+		System.out.println("chassisNumber");
+		System.out.println("isPunctured");
+		System.out.println("bikename");
+		System.out.println("runningkm");
+	}
+
+}
